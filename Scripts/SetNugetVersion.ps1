f5efd83c08bcb3ac87afb90150c8dbb550dddb9d
Bret Copeland <bret@atlantisflight.org>
2015-02-28 01:08:09 -0500

    NuGet config for AppVeyor.

---
+++ Scripts/SetNugetVersion.ps1
@@ -0,0 +1,30 @@
+
+# figure out the correct nuget package version (depends on whether this is a release or not)
+$version = "$env:nuget_version"
+if ("$env.APPVEYOR_REPO_TAG" -ne "true") # non-tagged (pre-release build)
+{
+  $version += "-build$env:APPVEYOR_BUILD_NUMBER"
+}
+
+# grab .nuspec file contents
+$file = "$PSScriptRoot\..\BosunReporter\BosunReporter.nuspec"
+$contents = (Get-Content $file) | Out-String
+
+# replace NUGET_VERSION with the the actual version
+$count = 0
+$replacer = [System.Text.RegularExpressions.MatchEvaluator]{
+  $script:count++
+  "$version"
+}
+$contents = [Regex]::Replace($contents, "NUGET_VERSION", $replacer)
+
+# make sure NUGET_VERSION existed exactly once
+if ($count -ne 1)
+{
+  Write-Error "NUGET_VERSION was found $count times in the nuspec file. If should have been found exactly once."
+  Exit 1
+}
+
+
+Write-Host "Nuget version set as $version"
+$contents | Set-Content $file
