1019bdd795d7255900dfbd2c28703f24f261f585
Bret Copeland <bret@atlantisflight.org>
2015-03-07 22:38:04 -0500

    Add SnapshotCounter.

---
+++ BosunReporter/Metrics/SnapshotCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using BosunReporter.Infrastructure;
+
+namespace BosunReporter.Metrics
+{
+    public class SnapshotCounter : BosunMetric
+    {
+        public readonly Func<long?> GetCountFunc;
+
+        public override string MetricType => "counter";
+
+        public SnapshotCounter(Func<long?> getCountFunc)
+        {
+            if (getCountFunc == null)
+                throw new ArgumentNullException("getCountFunc");
+
+            GetCountFunc = getCountFunc;
+        }
+
+        protected SnapshotCounter()
+        {
+        }
+
+        protected override IEnumerable<string> GetSerializedMetrics(string unixTimestamp)
+        {
+            var val = GetValue();
+            if (!val.HasValue)
+                yield break;
+
+            yield return ToJson("", val.Value, unixTimestamp);
+        }
+
+        protected virtual long? GetValue()
+        {
+            return GetCountFunc();
+        }
+    }
+}
