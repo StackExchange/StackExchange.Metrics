65c04afcc0625083f1662673312e767cde9ef5f4
Bret Copeland <bret@atlantisflight.org>
2015-12-11 16:35:49 -0500

    Fix line endings

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -42,9 +42,9 @@ namespace Scratch
                 Console.WriteLine(exception);
             };
 
-            collector.BeforeSerialization += () => Debug.WriteLine("BosunReporter: Running metrics snapshot.");
-            collector.AfterSerialization += info => Debug.WriteLine($"BosunReporter: Metric Snapshot took {info.MillisecondsDuration.ToString("0.##")}ms");
-            collector.AfterPost += info => Debug.WriteLine($"BosunReporter: {info.Count} metrics posted to Bosun in {info.MillisecondsDuration.ToString("0.##")}ms ({(info.Successful ? "SUCCESS" : "FAILED")})");
+            collector.BeforeSerialization += () => Console.WriteLine("BosunReporter: Running metrics snapshot.");
+            collector.AfterSerialization += info => Console.WriteLine($"BosunReporter: Metric Snapshot took {info.MillisecondsDuration.ToString("0.##")}ms");
+            collector.AfterPost += info => Console.WriteLine($"BosunReporter: {info.Count} metrics posted to Bosun in {info.MillisecondsDuration.ToString("0.##")}ms ({(info.Successful ? "SUCCESS" : "FAILED")})");
 
             collector.BindMetric("my_counter", "increments", typeof(TestCounter));
             var counter = collector.GetMetric<TestCounter>("my_counter", "increments", "This is meaningless.");
