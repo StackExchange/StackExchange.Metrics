423aabb0d4aac858aa5737d009c703e4c627d88f
Dean Ward <dward@stackoverflow.com>
2020-03-27 21:56:31 +0000

    Initial 1.1.0 bits
    ## .NET Core
    
    Adds extension methods for better integration with .NET Core startup - adding the metrics collector is now just a case of calling `AddMetricsCollector` in `Startup.ConfigureServices`, optionally calling methods on `IMetricsCollectorBuilder` to configure the `MetricsCollector` itself.
    
    ## Metric Sets
    
    Pre-defined sets of metrics that can be used to report on common framework-level metrics without having to implement them yourself. E.g.
    
    ### ProcessMetricSet
    
    Provides basic metrics for a .NET application. This set contains the following metrics:
    
     - `cpu.processortime` - total processor time in seconds
     - `cpu.threads` - threads for the process
     - `mem.virtual` - virtual memory for the process
     - `mem.paged` - paged memory for the process
    +### GarbageCollectionMetricSet (.NET Full Framework)
    
    Provides metrics about the garbage collector (GC). This set contains the following metrics:
    
     - `mem.collections_gen0` - number of gen-0 collections
     - `mem.collections_gen1` - number of gen-1 collections
     - `mem.collections_gen2` - number of gen-2 collections
    
    ### RuntimeMetricSet (.NET Core)
    
    Provides .NET Core runtime metrics which includes:
    
     - `cpu.usage` - % CPU usage
     - `mem.working_set` - working set of the process in bytes
     - `mem.size_heap` - total number of bytes across all heaps
     - `mem.size_gen0` - total number of bytes in gen-0
     - `mem.size_gen1` - total number of bytes in gen-1
     - `mem.size_gen2` - total number of bytes in gen-2
     - `mem.size_loh` - total number of bytes in the LOH
     - `mem.collection_gen0` - number of gen-0 collections
     - `mem.collection_gen1` - number of gen-1 collections
     - `mem.collection_gen2` - number of gen-2 collections
     - `threadpool.count` - number of threads in the threadpool
     - `threadpool.queue_length` - number of work items queued to the threadpool
     - `timers.count` - number of active timers
    
    ### AspNetMetricSet (.NET Core)
    
     - `kestrel.requests.pre_sec` - requests per second
     - `kestrel.requests.total` - total requests
     - `kestrel.requests.current` - current requests
     - `kestrel.requests.failed` - failed requests
    
    ## Sample App
    
    A sample .NET Core 3.1 app that configures the metrics collector and provides methods to dump state.

---
+++ docs/MetricSets.md
@@ -0,0 +1,56 @@
+# Metric Sets
+
+Metric sets are pre-packaged sets of metrics that are useful across different applications. These can be added to a `MetricsCollector` by specifying in the `IEnumerable<IMetricSet>` exposed at `MetricsCollectorOptions.Sets`.
+
+## Built-in sets
+
+### ProcessMetricSet
+
+Provides basic metrics for a .NET application. This set contains the following metrics:
+
+ - `cpu.processortime` - total processor time in seconds
+ - `cpu.threads` - threads for the process
+ - `mem.virtual` - virtual memory for the process
+ - `mem.paged` - paged memory for the process
+
+### GarbageCollectionMetricSet (.NET Full Framework)
+
+Provides metrics about the garbage collector (GC). This set contains the following metrics:
+
+ - `mem.collections_gen0` - number of gen-0 collections
+ - `mem.collections_gen1` - number of gen-1 collections
+ - `mem.collections_gen2` - number of gen-2 collections
+
+### RuntimeMetricSet (.NET Core)
+
+Provides .NET Core runtime metrics which includes:
+
+ - `cpu.usage` - % CPU usage
+ - `mem.working_set` - working set of the process in bytes
+ - `mem.size_heap` - total number of bytes across all heaps
+ - `mem.size_gen0` - total number of bytes in gen-0
+ - `mem.size_gen1` - total number of bytes in gen-1
+ - `mem.size_gen2` - total number of bytes in gen-2
+ - `mem.size_loh` - total number of bytes in the LOH
+ - `mem.collection_gen0` - number of gen-0 collections
+ - `mem.collection_gen1` - number of gen-1 collections
+ - `mem.collection_gen2` - number of gen-2 collections
+ - `threadpool.count` - number of threads in the threadpool
+ - `threadpool.queue_length` - number of work items queued to the threadpool
+ - `timers.count` - number of active timers
+
+### AspNetMetricSet (.NET Core)
+
+ - `kestrel.requests.pre_sec` - requests per second
+ - `kestrel.requests.total` - total requests
+ - `kestrel.requests.current` - current requests
+ - `kestrel.requests.failed` - failed requests
+
+## Custom Metric Sets
+
+Custom metric sets can be defined by implementing the `IMetricSet` interface. Implementors need to implement two methods:
+
+ - `Initialize` - this method is passed an `IMetricsCollector` and should be used to create metrics that the set defines. It can also be used to hook up long-lived metrics monitoring checks
+ - `Snapshot` - this method can be optionally implemented and is executed everytime the collector snapshots metrics for reporting
+
+ Once defined `IMetricSet` implementations should be added to the `MetricsCollectorOptions` that is passed to the `MetricsCollector`.
---
+++ src/StackExchange.Metrics/MetricsCollectorExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Metrics.Handlers;
+
+namespace StackExchange.Metrics
+{
+    /// <summary>
+    /// Extension methods for <see cref="MetricsCollector" />
+    /// </summary>
+    public static class MetricsCollectorExtensions
+    {
+        /// <summary>
+        /// Dumps the state of a <see cref="MetricsCollector" />.
+        /// </summary>
+        public async static Task DumpAsync(this MetricsCollector collector, TextWriter textWriter, string[] excludedTags = null)
+        {
+            excludedTags = excludedTags ?? Array.Empty<string>();
+            await textWriter.WriteLineAsync("DefaultTags:");
+            var defaultTags = collector.DefaultTags;
+            if (defaultTags.Count > 0)
+            {
+                foreach (var t in defaultTags)
+                {
+                    await textWriter.WriteAsync("  ");
+                    await textWriter.WriteAsync(t.Key);
+                    await textWriter.WriteAsync(" = ");
+                    await textWriter.WriteAsync(t.Value);
+                }
+            }
+
+            await textWriter.WriteLineAsync("");
+            await textWriter.WriteLineAsync("----");
+            await textWriter.WriteLineAsync("Endpoints:");
+
+            var endpoints = collector.Endpoints;
+            foreach (var endpoint in endpoints)
+            {
+                await textWriter.WriteAsync("  ");
+                switch (endpoint.Handler)
+                {
+                    case BosunMetricHandler bosunHandler:
+                        await textWriter.WriteLineAsync($"{endpoint.Name}: {bosunHandler.BaseUri?.AbsoluteUri ?? "null"}");
+                        break;
+                    case SignalFxMetricHandler signalFxHandler:
+                        await textWriter.WriteLineAsync($"{endpoint.Name}: {signalFxHandler.BaseUri?.AbsoluteUri ?? "null"}");
+                        break;
+                    case LocalMetricHandler h:
+                        await textWriter.WriteLineAsync($"{endpoint.Name}: Local");
+                        break;
+                }
+            }
+
+            await textWriter.WriteLineAsync("----");
+            await textWriter.WriteLineAsync("Counters:");
+
+            var count = 0;
+            var localHandler = endpoints.Select(x => x.Handler).OfType<LocalMetricHandler>().FirstOrDefault();
+            if (localHandler != null)
+            {
+                foreach (var reading in localHandler.GetReadings(reset: false).OrderBy(r => r.NameWithSuffix))
+                {
+                    await textWriter.WriteAsync("Name = ");
+                    await textWriter.WriteAsync(reading.Name);
+                    await textWriter.WriteAsync(reading.Suffix);
+                    await textWriter.WriteAsync(", Value = ");
+                    await textWriter.WriteAsync(reading.Value.ToString());
+                    await textWriter.WriteAsync(", Type = ");
+                    await textWriter.WriteAsync(reading.Type.ToString());
+                    await textWriter.WriteAsync(", Tags = [");
+
+                    foreach (var tag in reading.Tags)
+                    {
+                        // Skip excluded tags
+                        if (excludedTags.Contains(tag.Key))
+                        {
+                            continue;
+                        }
+
+                        await textWriter.WriteAsync(tag.Key);
+                        await textWriter.WriteAsync(" = ");
+                        await textWriter.WriteAsync(tag.Value);
+                        await textWriter.WriteAsync(", ");
+                    }
+
+                    await textWriter.WriteLineAsync("]");
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                await textWriter.WriteLineAsync("No readings found yet - this is normal immediately after startup, GIVE IT A MINUTE.");
+            }
+        }
+    }
+}
