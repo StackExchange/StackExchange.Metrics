452d4185dd027a1baa0ad73fb1c6bc3785c57e45
Dean Ward <dean.ward@bakedbean.org.uk>
2020-03-30 13:06:39 +0100

    Add support for specifying `MetricTag` on a read-only property
    Partially addresses #33

--- src/StackExchange.Metrics/Infrastructure/MetricTag.cs
+++ src/StackExchange.Metrics/Infrastructure/MetricTag.cs
@@ -8,7 +8,7 @@ namespace StackExchange.Metrics.Infrastructure
         public readonly string Name;
         public readonly bool IsFromDefault;
         public readonly bool IsOptional;
-        public readonly FieldInfo FieldInfo;
+        public readonly MemberInfo MemberInfo;
         public readonly MetricTagAttribute Attribute;
 
         /// <summary>
@@ -22,32 +22,50 @@ namespace StackExchange.Metrics.Infrastructure
         }
 
         /// <summary>
-        /// Use this constructor when instantiating from a field.
+        /// Use this constructor when instantiating from a field or property.
         /// </summary>
-        public MetricTag(FieldInfo fieldInfo, MetricTagAttribute attribute, Func<string, string> nameReplacer)
+        public MetricTag(MemberInfo memberInfo, MetricTagAttribute attribute, Func<string, string> nameReplacer)
         {
-            IsFromDefault = false;
-            IsOptional = attribute.IsOptional;
-
-            FieldInfo = fieldInfo;
-            if (!FieldInfo.IsInitOnly || (FieldInfo.FieldType != typeof(string) && !FieldInfo.FieldType.IsEnum))
+            switch (memberInfo)
             {
-                throw new InvalidOperationException(
-                    $"The BosunTag attribute can only be applied to readonly string or enum fields. {fieldInfo.DeclaringType.FullName}.{fieldInfo.Name} is invalid.");
+                case FieldInfo fieldInfo:
+                    if (!fieldInfo.IsInitOnly || (fieldInfo.FieldType != typeof(string) && !fieldInfo.FieldType.IsEnum))
+                    {
+                        throw new InvalidOperationException(
+                            $"The MetricTag attribute can only be applied to readonly string or enum fields. {memberInfo.DeclaringType.FullName}.{memberInfo.Name} is invalid."
+                        );
+                    }
+                    break;
+                case PropertyInfo propertyInfo:
+                    if (propertyInfo.SetMethod != null || (propertyInfo.PropertyType != typeof(string) && !propertyInfo.PropertyType.IsEnum))
+                    {
+                        throw new InvalidOperationException(
+                            $"The MetricTag attribute can only be applied to readonly string or enum properties. {memberInfo.DeclaringType.FullName}.{memberInfo.Name} is invalid."
+                        );
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException(
+                        $"The MetricTag attribute can only be applied to properties or fields. {memberInfo.DeclaringType.FullName}.{memberInfo.Name} is invalid."
+                    );
+
             }
 
+            IsFromDefault = false;
+            IsOptional = attribute.IsOptional;
+            MemberInfo = memberInfo;
             Attribute = attribute;
 
             if (attribute.Name != null)
                 Name = attribute.Name;
             else if (nameReplacer != null)
-                Name = nameReplacer(fieldInfo.Name);
+                Name = nameReplacer(memberInfo.Name);
             else
-                Name = fieldInfo.Name;
+                Name = memberInfo.Name;
 
             if (!MetricValidation.IsValidTagName(Name))
             {
-                throw new InvalidOperationException($"\"{Name}\" is not a valid Bosun Tag name. Field: {fieldInfo.DeclaringType.FullName}.{fieldInfo.Name}.");
+                throw new InvalidOperationException($"\"{Name}\" is not a valid tag name. Field: {memberInfo.DeclaringType.FullName}.{memberInfo.Name}.");
             }
         }
     }
